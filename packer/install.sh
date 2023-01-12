224d078fce742a3a6b9bfc681c1a5dd4eb25c591
AJGit98 <122079133+AJGit98@users.noreply.github.com>
2023-01-12 10:47:43 +0530

    Update install.sh

--- packer/install.sh
+++ packer/install.sh
@@ -24,8 +24,7 @@ sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/Ama
 sudo apt install -y unzip
 sudo unzip -o AmazonCloudWatchAgent.zip
 sudo ./install.sh
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-adi -s
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-adi-L2P6 -s
 systemctl start amazon-cloudwatch-agent.service
 systemctl enable amazon-cloudwatch-agent.service
 systemctl status amazon-cloudwatch-agent.service
